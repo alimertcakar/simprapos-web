276b1322077e06a0846dfc42b2f780df561ec901
Ali Mert Çakar <mail@alimert.net>
2021-09-11 02:10:18 +0300

    code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { IconsComponent } from './icons/icons.component';
 import { PinScreenComponent } from './pin-screen/pin-screen.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: PinScreenComponent }];
+const routes: Routes = [
+  { path: '', component: PinScreenComponent },
+  { path: 'ic', component: IconsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PinScreenComponent } from './pin-screen/pin-screen.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './headerr/header/header.component';
+import { IconsComponent } from './icons/icons.component';
 
 @NgModule({
-  declarations: [PinScreenComponent, AppComponent, HeaderComponent],
+  declarations: [
+    PinScreenComponent,
+    AppComponent,
+    HeaderComponent,
+    IconsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
