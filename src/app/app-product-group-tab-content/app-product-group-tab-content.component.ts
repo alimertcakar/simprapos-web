5890e5b49ff2fc4478f482fd10c117ab800ce3e5
Ali Mert Çakar <mail@alimert.net>
2021-09-11 12:50:21 +0300

    code

--- src/app/app-product-group-tab-content/app-product-group-tab-content.component.ts
+++ src/app/app-product-group-tab-content/app-product-group-tab-content.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { IProductGroup } from 'src/app/fastfood-orders/fastfood-orders.component';
 
 @Component({
-  selector: 'app-app-product-group-tab-content',
+  selector: 'app-product-group-tab-content',
   templateUrl: './app-product-group-tab-content.component.html',
   styleUrls: ['./app-product-group-tab-content.component.scss'],
 })
@@ -10,6 +10,8 @@ export class AppProductGroupTabContentComponent implements OnInit {
   @Input() productGroup!: IProductGroup;
   @Input() index!: number;
   @Input() activeIndex!: number;
+
+  activeSubgroup: number = 0;
 
   constructor() {}
 
