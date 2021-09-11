55ae085c3ed2dd4ce4197b35202d78a042d03197
Ali Mert Çakar <mail@alimert.net>
2021-09-11 06:20:24 +0300

    style

--- src/app/fastfood-orders/fastfood-orders.component.ts
+++ src/app/fastfood-orders/fastfood-orders.component.ts
@@ -1,166 +1,179 @@
 import { Component, OnInit } from '@angular/core';
 
+type IProductGroupName = string;
+type IProductSubGroupName = string;
+
+type IProductSubgroupVariantSelectType = 'MULTI' | 'SINGLE';
+
+interface IProductSubgroupVariant {
+  name: string;
+  selectType: IProductSubgroupVariantSelectType; // Tekli seçim - çoklu seçim.
+}
+
+interface IProductSubgroupItem {
+  name: string;
+  price: string | number;
+  currency: string;
+  variants?: IProductSubgroupVariant[];
+}
+
+interface IProductSubgroup {
+  name: IProductSubGroupName;
+  items: IProductSubgroupItem[];
+}
+
+interface IProductGroup {
+  name: IProductGroupName;
+  subgroup: IProductSubgroup[];
+}
+
 @Component({
   selector: 'app-fastfood-orders',
   templateUrl: './fastfood-orders.component.html',
   styleUrls: ['./fastfood-orders.component.scss'],
 })
-
-type IProductGroupName = string;
-type IProductSubGroupName = string;
-
-type IProductSubgroupVariantSelectType = "MULTI" | "SINGLE";
-
-interface IProductSubgroupVariant {
-  name:string;
-  selectType: IProductSubgroupVariantSelectType; // Tekli seçim - çoklu seçim.
-}
-
-interface IProductSubgroupItem {
-  name: string,
-  price: string | number,
-  currency: string;
-  variants?: IProductSubgroupVariant[];
-}
-
-interface IProductSubgroup{
-  name: IProductSubGroupName;
-  items: IProductSubgroupItem[];
-}
-
-interface IProductGroup{
-  name:IProductGroupName,
-  subgroup:IProductSubgroup[]
-}
-
 export class FastfoodOrdersComponent implements OnInit {
   productGroups: IProductGroup[] = [
-    { name: 'Etiketler', subgroup:[{
-      name:"Çok Satanlar",
-      items:[
+    {
+      name: 'Etiketler',
+      subgroup: [
         {
-          name:"1 Top Dondurma",
-          price:9,
-          currency:"TRY"
-       },
-       {
-        name:"Cola",
-        price:3,
-        currency:"TRY"
-      },
-      {
-        name:"Hamburger",
-        price:12,
-        currency:"TRY"
-      },
-      {
-         name:"Sebze Çorbası",
-         price:6,
-         currency:"TRY"
-      },
-      {
-        name:"Su",
-        price:2,
-        currency:"TRY"
-     }
-      ]
-    }]
-  },
-    { name: 'İçecekler', subgroup:[{
-      name:"Sıcak İçecekler",
-      items:[
-        {
-          name:"Americano",
-          price:4,
-          currency:"TRY"
-       },
-       {
-        name:"Caffe Latte",
-        price:3,
-        currency:"TRY"
-      },
-      {
-        name:"Espresso",
-        price:3.5,
-        currency:"TRY"
-      },
-      ]
-    }, {
-      name:"Soğuk İçecekler",
-      items:[
-        {
-          name:"Cola",
-          price:3,
-          currency:"TRY"
-       },
-       {
-        name:"Cola Light",
-        price:3,
-        currency:"TRY"
-      },
-      {
-        name:"Cola Zero",
-        price:3,
-        currency:"TRY"
-      },
-      {
-        name:"Su",
-        price:2,
-        currency:"TRY"
-      },
-      ]
-    }] },
-    { name: 'Yiyecekler',subgroup:[{
-      name:"Ana Yemekler",
-      items:[
-        {
-          name:"Americano",
-          price:4,
-          currency:"TRY"
-       },
-       {
-        name:"Caffe Latte",
-        price:3,
-        currency:"TRY"
-      },
-      {
-        name:"Espresso",
-        price:3.5,
-        currency:"TRY"
-      },
-      ]
-    }, {
-      name:"Çorbalar",
-      items:[
-        {
-          name:"Domates Çorbası",
-          price:6,
-          currency:"TRY"
-       },
-       {
-        name:"Sebze Çorbası",
-        price:6,
-        currency:"TRY"
-      },
-      ]
+          name: 'Çok Satanlar',
+          items: [
+            {
+              name: '1 Top Dondurma',
+              price: 9,
+              currency: 'TRY',
+            },
+            {
+              name: 'Cola',
+              price: 3,
+              currency: 'TRY',
+            },
+            {
+              name: 'Hamburger',
+              price: 12,
+              currency: 'TRY',
+            },
+            {
+              name: 'Sebze Çorbası',
+              price: 6,
+              currency: 'TRY',
+            },
+            {
+              name: 'Su',
+              price: 2,
+              currency: 'TRY',
+            },
+          ],
+        },
+      ],
     },
     {
-      name:"Tatlılar",
-      items:[
+      name: 'İçecekler',
+      subgroup: [
         {
-          name:"1 Top Dondurma",
-          price:9,
-          currency:"TRY"
-       },
-       {
-        name:"Tiramusu",
-        price:7.5,
-        currency:"TRY"
-      },
-      ]
-    }
-  ]
-   },
+          name: 'Sıcak İçecekler',
+          items: [
+            {
+              name: 'Americano',
+              price: 4,
+              currency: 'TRY',
+            },
+            {
+              name: 'Caffe Latte',
+              price: 3,
+              currency: 'TRY',
+            },
+            {
+              name: 'Espresso',
+              price: 3.5,
+              currency: 'TRY',
+            },
+          ],
+        },
+        {
+          name: 'Soğuk İçecekler',
+          items: [
+            {
+              name: 'Cola',
+              price: 3,
+              currency: 'TRY',
+            },
+            {
+              name: 'Cola Light',
+              price: 3,
+              currency: 'TRY',
+            },
+            {
+              name: 'Cola Zero',
+              price: 3,
+              currency: 'TRY',
+            },
+            {
+              name: 'Su',
+              price: 2,
+              currency: 'TRY',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Yiyecekler',
+      subgroup: [
+        {
+          name: 'Ana Yemekler',
+          items: [
+            {
+              name: 'Americano',
+              price: 4,
+              currency: 'TRY',
+            },
+            {
+              name: 'Caffe Latte',
+              price: 3,
+              currency: 'TRY',
+            },
+            {
+              name: 'Espresso',
+              price: 3.5,
+              currency: 'TRY',
+            },
+          ],
+        },
+        {
+          name: 'Çorbalar',
+          items: [
+            {
+              name: 'Domates Çorbası',
+              price: 6,
+              currency: 'TRY',
+            },
+            {
+              name: 'Sebze Çorbası',
+              price: 6,
+              currency: 'TRY',
+            },
+          ],
+        },
+        {
+          name: 'Tatlılar',
+          items: [
+            {
+              name: '1 Top Dondurma',
+              price: 9,
+              currency: 'TRY',
+            },
+            {
+              name: 'Tiramusu',
+              price: 7.5,
+              currency: 'TRY',
+            },
+          ],
+        },
+      ],
+    },
   ];
 
   constructor() {}
