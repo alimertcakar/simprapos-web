9bae439a50540b377c4e41252833cd8e3479e5ae
Ali Mert Çakar <mail@alimert.net>
2021-09-14 22:31:52 +0300

    style: register, inputs

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  goRegister() {
+    this.router.navigate(['/register']);
   }
 
+  ngOnInit(): void {}
 }
